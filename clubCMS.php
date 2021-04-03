06fab7a7440d2d230a87a8d4487b2a8af50b66a3
GoodFaut <70525789+GoodFaut@users.noreply.github.com>
2021-04-03 20:53:08 +0200

    Add MarkDown Opener

--- clubCMS.php
+++ clubCMS.php
@@ -1,5 +1,9 @@
 <?php
+// Open Home.md
 $myfile = fopen("home.md", "r") or die("Unable to connect to echo $_SERVER['REQUEST_URI']");
-echo fread($myfile,filesize("webdictionary.txt"));
-fclose($myfile);
+// Converting MarkDown to HyperText Page.
+echo <div class="someclass" id="someid" markdown="1">
+    <?php fread($myfile,filesize("home.md")); ?>
+</div>
+ fclose($myfile);
 ?>
