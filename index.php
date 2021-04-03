e09cd9872cd8c1f251baee18fe8d13960d01df74
GoodFaut <70525789+GoodFaut@users.noreply.github.com>
2021-04-03 20:42:57 +0200

    Adding heading and footer.

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-
+<?php
+// (c) 2021 ClubCMS
+// This is ClubCMS
+// Include the header of your website
+include 'header.php';
+// Include the ClubCMS content ( blog, about, pricing...).
+include 'clubCMS.php';
+// Include footer of your website (ex: (c)2021 ClubCMS)
+include 'footer.php';
+?>
