83ea5559931825b22261334c4fb555f46ffa3ca1
GoodFaut <70525789+GoodFaut@users.noreply.github.com>
2021-04-04 10:05:12 +0200

    Add classes and id.

--- clubCMS.php
+++ clubCMS.php
@@ -2,7 +2,7 @@
 // Open Home.md
 $myfile = fopen("home.md", "r") or die("Unable to connect to echo $_SERVER['REQUEST_URI']");
 // Converting MarkDown to HyperText Page.
-echo <div class="someclass" id="someid" markdown="1">
+echo <div class="markdown" id="home" markdown="1">
     <?php fread($myfile,filesize("home.md")); ?>
 </div>
  fclose($myfile);
