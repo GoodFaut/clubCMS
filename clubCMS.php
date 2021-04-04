63f7b73b117ef13a851566480586f89e9b45f278
GoodFaut <70525789+GoodFaut@users.noreply.github.com>
2021-04-04 11:33:29 +0200

    Add title

--- clubCMS.php
+++ clubCMS.php
@@ -1,8 +1,10 @@
 <?php
 // Open Home.md
-$myfile = fopen("home.md", "r") or die("Unable to connect to echo $_SERVER['REQUEST_URI']");
+$myfile = fopen("home.md", "r") or die("Unable to connect to website content.");
 // Converting MarkDown to HyperText Page.
-echo <div class="markdown" id="home" markdown="1">
+echo 
+  <title> ClubCMS </title>
+    <div class="markdown" id="home" markdown="1">
     <?php fread($myfile,filesize("home.md")); ?>
 </div>
  fclose($myfile);
