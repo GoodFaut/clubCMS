c643fc57aece2cf84b7f25a6815b54d7e78311a8
GoodFaut <70525789+GoodFaut@users.noreply.github.com>
2021-04-04 10:24:17 +0200

    Updated

--- header.php
+++ header.php
@@ -1,1 +1,9 @@
-echo Pendiente
+<?php
+// Open Header.md
+$myfile = fopen("header.md", "r") or die("Unable to connect to echo $_SERVER['REQUEST_URI']");
+// Converting MarkDown to HyperText Page.
+echo <div class="markdown" id="home" markdown="1">
+    <?php fread($myfile,filesize("header.md")); ?>
+</div>
+ fclose($myfile);
+?>
