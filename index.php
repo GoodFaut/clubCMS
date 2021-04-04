05ce6aca3bc55bfc40d8850e4f6082ce68155804
GoodFaut <70525789+GoodFaut@users.noreply.github.com>
2021-04-04 12:53:18 +0200

    Add HOME.MD

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 // Include the header of your website
 include 'header.php';
 // Include the ClubCMS content ( blog, about, pricing...).
-include 'clubCMS.php';
+include clubCMS.php?url=home.md';
 // Include footer of your website (ex: (c)2021 ClubCMS)
 include 'footer.php';
 ?>
